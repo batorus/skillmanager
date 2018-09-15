4749b30d74fa70594e01602a22d0570996a9cf39
batorus <batoru@yahoo.com>
2018-09-15 20:53:53 +0300

    wip

---
+++ database/migrations/2018_09_15_174547_create_skills_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSkillsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('skills', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned()->index();
+            $table->integer('domain_id')->unsigned();
+            $table->integer('level_id');
+            $table->string('date_recorded');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('skills');
+    }
+}
