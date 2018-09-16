926c7ca9f2211667a7a58472a0c952b6a2dbd87c
batorus <batoru@yahoo.com>
2018-09-16 18:17:11 +0300

    wip

--- app/Http/Controllers/ChartingController.php
+++ app/Http/Controllers/ChartingController.php
@@ -19,10 +19,34 @@ class ChartingController extends Controller
      public function indexAction(\App\User $user)
     {
 
+         
+         $data = $this->skills->forUserAndDomain($user, 3);
+         
+         $dates = [];
+         foreach($data as $d){
+             
+           $dates[] = array('date'=>$d->date_recorded);
+         }
+         //dd(json_encode($dates));
+        // strtotime("11-09-2018");
+//          $php_date = getdate(strtotime("11-09-2018"));
+//          // or if you want to output a date in year/month/day format:
+//         $date = date("Y/m/d", strtotime("11-09-2018")); 
+//         
+//         $yr = date("Y", strtotime("11-09-2018")) ; 
+//        $mon = date("m", strtotime("11-09-2018")); 
+//        $date = date("d", strtotime("11-09-2018")); 
+//        $dateFormat = 'm-d-Y';
+//        $stringDate = '2000-11-29';
+//        $date = DateTime::createFromFormat($dateFormat, $stringDate);   
+//        dd($mon);
         return view('charts.index', 
                 [
                     'skills' => $this->skills->forUserAndDomain($user, 3),
+                    'dates'  => json_encode($dates),
                     'user' => $user
                 ]);
+        
+        //return response()->json($this->skills->forUserAndDomain($user, 3));
     }
 }
--- resources/views/charts/index.blade.php
+++ resources/views/charts/index.blade.php
@@ -2,61 +2,114 @@
 
 @section('content')
 
-    <div class="panel-body">
+<div class="table-responsive col-md-12" >
+    <div id="container" ></div>
 
-       
-
- @if (count($skills) > 0)
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                Current Skills
-            </div>
-
-            <div class="panel-body">
-                <table class="table table-striped task-table">
-
-                    <!-- Table Headings -->
-                    <thead>
-                        <th>User name</th>
-                        <th>Skill name</th>
-                        <th>Skill level</th>    
-                        <th>Date</th>  
-                        <th>&nbsp;</th>
-                    </thead>
-
-                    <!-- Table Body -->
-                    <tbody>
-                        @foreach ($skills as $skill)
-                            <tr>
-                                <td class="table-text">
-                                    <div>{{ $skill->username}}</div>
-                                </td>
-                                <td class="table-text">
-                                    <div>{{ $skill->namedomain }}</div>
-                                </td>
-                                <td class="table-text">
-                                    <div>{{ $skill->namelevel }}</div>
-                                </td>
-                                <td class="table-text">
-                                    <div>{{ $skill->date_recorded}}</div>
-                                </td>
-                                <td>
-
-                                </td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    @endif
+</div>
 
     @section('javascripts')
       @parent
         <script type="text/javascript">
-            $( document ).ready(function() {
+        $( document ).ready(function() {
 
+                var skills = {!! json_encode($skills->toArray()) !!};
+                var dates= {!! $dates !!};
+                
+                //d = $.parseJSON(dates);
+                //console.log(dates);
+                
+                $(dates).each(function (i, val) 
+                {
+                     console.log(dates[i].date);    
+//                                  $("select#nomsociete_pc option[value="+val.id+"]").prop("selected", true).prop('disabled',false);
+//                                 // $("select#typesociete option[value="+val.typesociete+"]").prop("selected", true).prop('disabled',false);
+//                                  $("select#comptablesenior_pc option[value="+val.comptablesenior+"]").prop("selected", true).prop('disabled',false);
+//                                  $("select#comptablemanageur_pc option[value="+val.comptablemanageur+"]").prop("selected", true).prop('disabled',false);     
+//                                  $("select#site_pc option[value="+val.site+"]").prop("selected", true).prop('disabled',false); 
+                             
+                })
+                //console.log(dates);
+                Highcharts.chart('container', {
+                        chart: {
+                            type: 'spline'
+                        },
+                        title: {
+                            text: 'Denumire Domeniu aici'
+                        },
+                        subtitle: {
+                            text: 'Descriere scurta'
+                        },
+                        xAxis: {
+                            type: 'datetime',
+                            dateTimeLabelFormats: { // don't display the dummy year
+                                month: '%e. %b',
+                                year: '%b'
+                            },
+                            title: {
+                                text: 'Date'
+                            }
+                        },
+                        yAxis: {
+                            title: {
+                                text: 'Level'
+                            },
+                             min: 1,
+//                            max: 5,
+  //                          categories: [1, 2, 3, 4, 5],
+                            allowDecimals: false
+                        },
+                        tooltip: {
+                            headerFormat: '<b>{series.name}</b><br>',
+                            pointFormat: '{point.x:%e. %b}: {point.y:.2f} m'
+                        },
+
+                        plotOptions: {
+                            spline: {
+                                marker: {
+                                    enabled: true
+                                }
+                            }
+                        },
+
+                        colors: ['#6CF', '#39F', '#06C', '#036', '#000'],
+
+                        // Define the data points. All series have a dummy year
+                        // of 1970/71 in order to be compared on the same x axis. Note
+                        // that in JavaScript, months start at 0 for January, 1 for February etc.
+                        series: [{
+                            name: "Evolutie skill UNIX",
+                            data: [
+                                [Date.UTC(1970, 10, 25), 0],
+                                [Date.UTC(1970, 11,  6), 0.25],
+                                [Date.UTC(1970, 11, 20), 1.41],
+                                [Date.UTC(1970, 11, 25), 1.64],
+                                [Date.UTC(1971, 0,  4), 1.6],
+                                [Date.UTC(1971, 0, 17), 2.55],
+                                [Date.UTC(1971, 0, 24), 2.62],
+                                [Date.UTC(1971, 1,  4), 2.5],
+                                [Date.UTC(1971, 1, 14), 2.42],
+                                [Date.UTC(1971, 2,  6), 2.74],
+                                [Date.UTC(1971, 2, 14), 2.62],
+                                [Date.UTC(1971, 2, 24), 2.6],
+                                [Date.UTC(1971, 3,  1), 2.81],
+                                [Date.UTC(1971, 3, 11), 2.63],
+                                [Date.UTC(1971, 3, 27), 2.77],
+                                [Date.UTC(1971, 4,  4), 2.68],
+                                [Date.UTC(1971, 4,  9), 2.56],
+                                [Date.UTC(1971, 4, 14), 2.39],
+                                [Date.UTC(1971, 4, 19), 2.3],
+                                [Date.UTC(1971, 5,  4), 2],
+                                [Date.UTC(1971, 5,  9), 1.85],
+                                [Date.UTC(1971, 5, 14), 1.49],
+                                [Date.UTC(1971, 5, 19), 1.27],
+                                [Date.UTC(1971, 5, 24), 0.99],
+                                [Date.UTC(1971, 5, 29), 0.67],
+                                [Date.UTC(1971, 6,  3), 0.18],
+                                [Date.UTC(1971, 6,  4), 0]
+                            ]
+                        }]
             });
+        });
         </script>   
     @endsection
     
