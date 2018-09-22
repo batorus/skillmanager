4a6dfe2e5a975bd61766572885b8969044984ee7
batorus <batoru@yahoo.com>
2018-09-23 00:23:50 +0300

    wip solve disable state for pagination

--- resources/views/pagination/default.blade.php
+++ resources/views/pagination/default.blade.php
@@ -1,21 +1,25 @@
 @if ($paginator->lastPage() > 1)
 <ul class="pagination">
-    <li class="{{ ($paginator->currentPage() == 1) ? ' disabled' : '' }}">
-        <a href="{{ $paginator->url(1) }}" style="padding:5px;color:#fff; background-color: #0066cc">Prev</a>
+    <li >
+        <a href="{{ $paginator->url(1) }}" class="{{ ($paginator->currentPage() == 1) ? ' btn btn-primary disabled' : 'btn btn-primary' }}">Prev</a>
     </li>
     <span>&nbsp;</span>  
     
     @for ($i = 1; $i <= $paginator->lastPage(); $i++)
     <span>&nbsp;</span>
         <li class="{{ ($paginator->currentPage() == $i) ? 'active' : '' }}">
-            <a href="{{ $paginator->url($i) }}"  style="padding:5px;color:#fff; background-color: #0066cc" >{{ $i }}</a>
+            <a href="{{ $paginator->url($i) }}"  class='btn btn-primary' >{{ $i }}</a>
         </li>
     <span>&nbsp;</span>
     @endfor
     
     <span>&nbsp;</span>   
-    <li class="{{ ($paginator->currentPage() == $paginator->lastPage()) ? ' disabled' : '' }}">
-        <a href="{{ $paginator->url($paginator->currentPage()+1) }}" style="padding:5px;color:#fff; background-color: #0066cc">Next</a>
+    <li >
+        <a href="{{ $paginator->url($paginator->currentPage()+1) }}"  
+           class="{{ ($paginator->currentPage() == $paginator->lastPage()) ? 'btn btn-primary disabled' : 'btn btn-primary' }}"  
+           >
+            Next
+        </a>
     </li>
 </ul>
 @endif
